52d7f177756c194d4f507b080a28ef671ab6be98
Romie Hecdivert <65568062+rh9891@users.noreply.github.com>
2020-11-12 23:11:59 -0500

    Outlined the functionality of the application. Added event listeners.

---
+++ script.js
@@ -0,0 +1,21 @@
+const video = document.getElementById("video");
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timestamp");
+
+// Event listener to play the video when it is paused and pause the video when it is playing.
+video.addEventListener("click", toggleVideoStatus);
+// Event listener that updates the pause icon to change into a play icon when video is being paused.
+video.addEventListener("pause", updatePlayIcon);
+// Event listener that updates the play icon to change into a pause icon when video is being played.
+video.addEventListener("play", updatePlayIcon);
+// Event listener that is continuously called to update the time and progress of the video.
+video.addEventListener("timeupdate", updateProgress);
+
+play.addEventListener("click", toggleVideoStatus);
+
+stop.addEventListener("click", stopVideo);
+
+// Event listener (change event) for the progress bar to go to that point in the video that the user clicks to.
+progress.addEventListener("change", setVideoProgress);
